85f52363289dd8e91d8778f43560485d8517d219
Mateo Bernal Montoya <mbernalm@everis.com>
2019-02-28 23:00:05 +0100

    readyd for submit

--- parser.js
+++ parser.js
@@ -1,45 +1,167 @@
 const fs = require('fs');
 
+const _ = require('underscore');
+
+let slides = [];
+let verticalPhotos = [];
+let magicArray = [];
+let finalSlides = [];
 let N = 0;
+let map = {};
+let data = [];
+
+const addVerticalSlides = (p1, p2) => {
+  const newSlide = { ID: `${p1.ID} ${p2.ID}`, tags: _.union(p1.tags, p2.tags) };
+  slides.push(newSlide);
+}
+
+const addHorizontalSlides = (photo1) => {
+  slides.push(photo1);
+}
+
+const createMagicArray = () => {
+  for (i = 0; i < verticalPhotos.length; i++) {
+    magicArray[i] = [];
+    for (j = i + 1; j < verticalPhotos.length; j++) {
+      const totalTags = _.union(verticalPhotos[i].tags, verticalPhotos[j].tags)
+      if (totalTags.length < 7) {
+        magicArray[i][j] = totalTags;
+      }
+    }
+  }
+}
+
+const createVerticalSlides = () => {
+  for (i = 0; i < verticalPhotos.length; i++) {
+    if (verticalPhotos[i] && verticalPhotos[i+1]) {
+      addVerticalSlides(verticalPhotos[i], verticalPhotos[i + 1]);
+    }
+    i += 2;
+  }
+}
+
+const valuation = (slide1, slide2) => {
+  const common = _.intersection(slide1.tags, slide2.tags).length;
+
+  return Math.min(common, slide1.tags.length - common, slide2.tags.length - common);
+};
+const result=[];
+const joinSlides = () => {
+  
+  let totalPoints = 0;
+  let current;
+  let maxPoints =0;
+  let position =0;
+  do {
+    if (!result.length) {
+      current = slides[0]
+      slides.splice(0,1);
+    } else {
+      for (let j = 0; j < slides.length; j++) { 
+        if (!current || !slides[j]) {
+          slides = [];
+          break;
+        }
+        let points = valuation(current, slides[j]);
+        //console.log("points", points)
+        if (points > maxPoints) {
+          maxPoints = points;
+          position = j;
+        }
+        if (maxPoints > 2) break
+      }
+      totalPoints += maxPoints;
+      maxPoints=0;
+      current = slides[position];
+      //console.log(position)
+      slides.splice(position,1);
+      
+    }
+    result.push(current);
+//console.log(slides.length)
+  } while (slides.length)
+  //console.log(result.length)
+  console.log("Points: "+totalPoints)
+} 
+
+const processPhotoType = (photo) => {
+  if (photo.type === 'H') {
+    addHorizontalSlides(photo);
+  } else {
+    return photo;
+  }
+}
+
+const processPhoto = (photo) => {
+  const verticalPhoto = processPhotoType(photo);
+  if (verticalPhoto) {
+    verticalPhotos.push(verticalPhoto);
+  }
+}
 
 const getParams = () => {
   return N;
 };
 
-const parsePhotoData = (inputFile) => {
-  const data = fs.readFileSync(inputFile).toString();
-  let photos = [];
-  let i = 0;
-  while (data.charAt(i) !== "\n" && data.charAt(i) !== "\r") i++;
-  N = parseInt(data.substring(0, i), 10);
-  console.log("No. of photos (N): " + N);
-  if(data.charAt(i) == "\r") {pos = i+2; i = i+2;}
-  else pos = ++i;
-  i = 0;
-  while (i < N) {
-      let type = data.charAt(pos);
-      pos = pos + 2;
-      let j = 0;
-      while(data.charAt(pos+j)!== ' ') j++;
-      let nTags = parseInt(data.substring(pos,pos+j));
-      pos = pos + j + 1;
-      let tags = [];
-      for(t=0; t<nTags; t++){
-          let k = 0;
-          while(!(data.charAt(pos+k) == ' ' || data.charAt(pos+k)== '\n' || data.charAt(pos+k)== '\r')) k++;
-          let tag = data.substring(pos, pos+k);
-          if (data.charAt(pos+k) == '\r') pos = pos + k + 2;
-          else pos = pos+k + 1;
-          tags.push(tag);
+const parsePhotoData = async (inputFile) => {
+  return new Promise((resolve) => {
+    var lineReader = require('readline').createInterface({
+      input: require('fs').createReadStream(inputFile)
+    });
+var i = 0;
+    lineReader.on('line', function (line) {
+      i++;
+      let el = line.split(" ");
+      if (el.length < 2) {
+        return
       }
-      // console.log(nTags);
-    i++;
-    photos.push({ID: i, type: type, nTags: nTags, tags: tags});
-  }
-  return photos;
+      let tags = _.rest(el, 2);
+
+      tags = tags.map(element => {
+        if (!map[element]) {
+          map[element] = Object.keys(map).length + 1;
+        }
+        return map[element];
+      });
+      const photo = { ID: i, type: el[0], ntags: el[1], tags };
+      processPhoto(photo);
+      data.push(photo);
+    });
+
+    lineReader.on('close', () => { resolve(data) })
+  })
+
 };
 
-parsePhotoData(process.argv[2]);
+const outputParser = () => {
+  // var logger = fs.createWriteStream('log.txt', {
+  //   flags: 'a' // 'a' means appending (old data will be preserved)
+  // })
+  // logger.write(result.length);
+  fs.appendFileSync('log.txt', result.length + "\n") 
+
+  //console.log(result[0])
+  result.forEach((slide) => {
+    if (slide && slide.ID) {
+      fs.appendFileSync('log.txt', slide.ID + "\n") 
+    }
+    
+
+  });
+  // logger.end();
+}
+
+
+const main = async () => {
+  // parseamos la informacion, procesando cada foto
+  const data1 = await parsePhotoData(process.argv[2]);
+  createVerticalSlides();
+  joinSlides();
+  outputParser();
+  return slides;
+}
+
+main();
 
 module.exports = {
   parsePhotoData,
